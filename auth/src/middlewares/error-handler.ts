73fe84d8bb0ea861a47e1a7d3b08502f72d6edcf
robson01jc <robson01jc@gmail.com>
2025-09-15 09:09:58 -0300

    error common structure

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
-import { Request, Response, NextFunction } from 'express'
+import { NextFunction, Request, Response } from 'express'
+import { DatabaseConnectionError } from '../errors/database-connection-error'
+import { RequestValidationError } from '../errors/request-validation-error'
 
 export const errorHandler = (
   err: Error,
@@ -6,9 +8,15 @@ export const errorHandler = (
   res: Response,
   next: NextFunction,
 ) => {
-  console.log('Something went wrong', err)
+  if (err instanceof RequestValidationError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() })
+  }
 
-  res.status(400).send({
-    message: err.message
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() })
+  }
+
+  res.status(500).send({
+    errors: [{ message: 'Something went wrong' }]
   })
 }
