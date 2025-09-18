6e4c8ebc82173dabec88f66a0aa4db913e892445
robson01jc <robson01jc@gmail.com>
2025-09-18 09:05:14 -0300

    mongo pod created

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import 'express-async-errors'
 
 import { NotFoundError } from './errors/not-found-error'
 import { errorHandler } from './middlewares/error-handler'
@@ -16,7 +17,7 @@ app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
 
-app.all('*', () => {
+app.all('*', async () => {
   throw new NotFoundError()
 })
 
