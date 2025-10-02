4c303ab41ecdb18e79356df09061fb3d25ce95d2
robson01jc <robson01jc@gmail.com>
2025-10-02 09:08:09 -0300

    add cookie-session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,3 +1,4 @@
+import cookieSession from 'cookie-session'
 import express from 'express'
 import 'express-async-errors'
 import mongoose from 'mongoose'
@@ -10,8 +11,15 @@ import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
 
 const app = express()
+app.set('trust proxy', true)
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+)
 
 app.use(currentUserRouter)
 app.use(signinRouter)
